f9a213e584347b5672605c504bd44b2e76f12ec9
Nabil M. Al-Rousan <rousani@gmail.com>
2022-02-09 20:58:25 -0800

    Initial commit

---
+++ Projects/ch6_6.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+    cout<< "32432";
+      cout<< "32432";
+      int x = 5;
+      int rr[5] = {1, 2,3,4, 5};
+      x = x + 4;
+        cout<< "32432";
+}
